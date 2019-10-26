275d945e00d79d99268ae7a163fc58ea2a4db7a4
Ricky dildy <rdildy92@gmail.com>
2019-10-26 01:42:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,10 +1,11 @@
 
-require.pry
+require 'pry'
 def badge_maker(name)
   "Hello, my name is #{name}."
 end 
 
 def batch_badge_creator(attendees)
   attendees.each do |name|
+    binding.pry 
     badge_maker(name)
 end
