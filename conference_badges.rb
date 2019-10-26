bc32acad8318188483c35400417063cd7aee8970
Ricky dildy <rdildy92@gmail.com>
2019-10-26 02:10:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,7 @@ end
 def assign_rooms(attendees)
   attendees.collect_with_index(1) do |attendee, index| 
     binding.pry
-    "Hello,! You'll be assigned to room 1!"  
+    "Hello, #{attendee}! You'll be assigned to room #{index}!"  
   end
   # attendees.each_with_index do |attendee, index|
   #   binding.pry 
